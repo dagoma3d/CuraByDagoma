532ef5e6bdee2df64973517de792fe6407c9f307
Orel <aurelien@dagoma.fr>
2018-10-17 16:51:09 +0200

    Add a tooltip on "Add pause" label.

--- Cura/gui/pausePluginPanel.py
+++ Cura/gui/pausePluginPanel.py
@@ -22,8 +22,10 @@ class pausePluginPanel(wx.Panel):
 
 		lPauseTitle = wx.StaticText(self, -1, _("Add a pause")) # (92, 27) is the default size of a TextCtrl and (80, 17) is the default size of a StaticText
 		addLayerButton = wx.Button(self, id=-1, label="+", style=wx.BU_EXACTFIT)
-		lPauseTitleToolTip = wx.ToolTip(_("Add a pause at the layer selected in the 3D view"))
-		addLayerButton.SetToolTip(lPauseTitleToolTip)
+		addLayerButtonToolTip = wx.ToolTip(_("Add a pause at the layer selected in the 3D view"))
+		addLayerButton.SetToolTip(addLayerButtonToolTip)
+		lPauseTitleToolTip = wx.ToolTip(_("If you don't have any pause button on your printer...\nJust push the X endstop to resume your print!"))
+		lPauseTitle.SetToolTip(lPauseTitleToolTip)
 		sb = wx.StaticBox(self)
 
 		boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
