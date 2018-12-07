f29d1785e81a977cb28bdbc28b4933a5b342fa10
Orel <orel@dagoma.fr>
2018-12-07 12:47:16 +0100

    Review filaments stuff and rework a bit start/end/switch gocdes.

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 # CONFIGURATION
 #############################
 export RELEASE_VERSION=2.1.0
-export BUILD_VERSION=${RELEASE_VERSION}a9
+export BUILD_VERSION=${RELEASE_VERSION}
 
 ##Select the build target
 ##Available options:
