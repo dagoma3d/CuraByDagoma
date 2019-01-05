d6334a54067ebbe617fab1766fd53ad04df57995
orel <orel@dagoma.fr>
2019-01-05 12:01:57 +0100

    Increment alpha version.

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 # CONFIGURATION
 #############################
 export RELEASE_VERSION=2.1.1
-export BUILD_VERSION=${RELEASE_VERSION}a3
+export BUILD_VERSION=${RELEASE_VERSION}a4
 
 ##Select the build target
 ##Available options:
