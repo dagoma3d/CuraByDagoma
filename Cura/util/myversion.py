c8c92f237ffaeb4aedcaf3e04283ae12f9472508
orel <orel@dagoma.fr>
2019-01-05 11:14:12 +0100

    Enhance error handling when checking official version.

--- Cura/util/myversion.py
+++ Cura/util/myversion.py
@@ -6,11 +6,23 @@ __copyright__ = "Copyright (C) 2019 Dagoma - Released under terms of the AGPLv3 
 import os
 import urllib2
 
-official_version = None
+url_handle = None
+version_url = "https://dist.dagoma3d.com/version/CuraByDagoma"
+
 try:
-	official_version = urllib2.urlopen("https://dist.dagoma3d.com/version/CuraByDagoma").read()
+	url_handle = urllib2.urlopen(version_url)
+except urllib2.URLError:
+	try:
+		import ssl
+		context = ssl._create_unverified_context()
+		url_handle = urllib2.urlopen(version_url, context=context)
+	except:
+		pass
 except:
 	pass
 
 def isLatest():
-	return official_version is None or official_version == os.environ['CURABYDAGO_RELEASE_VERSION']
+	if url_handle is None:
+		return True
+	official_version = url_handle.read()
+	return official_version == os.environ['CURABYDAGO_RELEASE_VERSION']
