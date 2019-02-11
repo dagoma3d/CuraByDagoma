917b04865f18455ea3ebbae6f389ab82b5e495a8
orel <orel@dagoma.fr>
2019-02-11 20:59:43 +0100

    Set new release version.

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 # CONFIGURATION
 #############################
 export RELEASE_VERSION=2.1.2
-export BUILD_VERSION=${RELEASE_VERSION}a3
+export BUILD_VERSION=${RELEASE_VERSION}
 
 ##Select the build target
 ##Available options:
