ce2a2f528c82163b59e1f17bd0901978d99872bf
Orel <orel@dagoma.fr>
2019-09-09 09:57:55 +0200

    End of line when adding pause.

--- plugins/pauseAtZ-ByDagoma.py
+++ plugins/pauseAtZ-ByDagoma.py
@@ -91,11 +91,11 @@ with open(filename, "w") as f:
 				z = layerZ
 
 				pauseState = 0
-				f.write(";TYPE:CUSTOM\r\n")
+				f.write(';TYPE:CUSTOM\n')
 				#Retract
 				if profile.getMachineSetting('machine_name') in ['Neva', 'Magis']:
-					f.write("M600 U-55 X55 Y-92 Z60\r\n")
+					f.write('M600 U-55 X55 Y-92 Z60\n')
 				else:
-					f.write("M600 L0 PA\r\n")
+					f.write('M600 L0 PA\n')
 
 		f.write(line)
