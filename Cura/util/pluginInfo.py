da9fcb608cd5e6f2845fc0acf5086839ea98ebe5
orel <orel@dagoma.fr>
2021-02-16 14:32:32 +0100

    Restore pickle usage instead of json when playing with plugins params

--- Cura/util/pluginInfo.py
+++ Cura/util/pluginInfo.py
@@ -10,7 +10,7 @@ import traceback
 import platform
 import re
 import tempfile
-import json
+import pickle
 
 from Cura.util import profile
 from Cura.util import resources
@@ -72,13 +72,13 @@ class pluginInfo(object):
 
 def getPostProcessPluginConfig():
 	try:
-		return json.loads(profile.getProfileSetting('plugin_config'))
+		return pickle.loads(eval(profile.getProfileSetting('plugin_config')))
 	except Exception as e:
 		print(e)
 		return []
 
 def setPostProcessPluginConfig(config = []):
-	profile.putProfileSetting('plugin_config', json.dumps(config))
+	profile.putProfileSetting('plugin_config', pickle.dumps(config))
 
 def getPluginBasePaths():
 	ret = []
