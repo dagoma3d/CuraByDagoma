87a1483fe50d9100bb9ff84c823670c622a7596f
Alutulu <arthur.mata24@gmail.com>
2022-05-27 16:57:31 +0200

    Incomplete solution, not satisfying for the moment

---
+++ cura.bat
@@ -0,0 +1,42 @@
+@echo off
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
+)
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" 65001 > nul
+)
+
+set VIRTUAL_ENV=%cd%\venv
+
+if not defined PROMPT set PROMPT=$P$G
+
+if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
+if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
+
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(venv) %PROMPT%
+
+if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
+
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+
+start "test" /i /MIN python.exe -m Cura.cura
+@REM start "test" /i /MIN python.exe -m Cura.cura
+rem best solution found for the moment
+rem /b to execute in background, but put cmd.exe on foreground
+rem j'ai essayé aussi de créer un autre fichier .bat qui appelle cura.bat, sans succès
+rem un DOS est créé à chaque fois qu'on double-clique sur un fichier bat
+
+@REM exit /b
+
+:END
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
+    set _OLD_CODEPAGE=
+)
--- scripts/windows/cura.bat
+++ scripts/windows/cura.bat
@@ -2,3 +2,4 @@ Set PYTHONPATH=%cd%\python\Lib
 Set PYTHONHOME=%cd%\python
 @python\python.exe -m Cura.cura %*
 
+REM maybe this file can be deleted
