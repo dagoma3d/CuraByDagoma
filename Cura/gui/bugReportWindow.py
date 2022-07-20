0a9899542c05b1c7ca1e75b06406130aca773373
Alutulu <arthur.mata24@gmail.com>
2022-07-20 14:10:00 +0200

    start window creation

--- Cura/gui/bugReportWindow.py
+++ Cura/gui/bugReportWindow.py
@@ -5,7 +5,7 @@ from Cura.util import resources
 
 class bugReportWindow(wx.Frame):
 	def __init__(self, parent):
-		super(bugReportWindow, self).__init__(parent, title=_("Shortcuts"), style = wx.DEFAULT_DIALOG_STYLE|wx.FRAME_FLOAT_ON_PARENT)
+		super(bugReportWindow, self).__init__(parent, title=_("Report a bug"), style = wx.DEFAULT_DIALOG_STYLE|wx.FRAME_FLOAT_ON_PARENT)
 
 		frameicon = wx.Icon(resources.getPathForImage('cura.ico'), wx.BITMAP_TYPE_ICO)
 		self.SetIcon(frameicon)
@@ -20,47 +20,14 @@ class bugReportWindow(wx.Frame):
 		s = wx.BoxSizer(wx.VERTICAL)
 		p.SetSizer(s)
 
-		title = wx.StaticText(p, -1, _("Shortcuts"))
+		title = wx.StaticText(p, -1, _("How to report a problem ?"))
 		title.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
 		s.Add(title, flag=wx.ALIGN_CENTRE|wx.BOTTOM|wx.TOP|wx.LEFT|wx.RIGHT, border=5)
-		s.Add(wx.StaticText(p, -1, _('Here is a list of shortcuts to help moving camera through the scene and applying transformations with the tools.')), flag=wx.ALIGN_CENTER|wx.BOTTOM|wx.LEFT|wx.RIGHT, border=5)
-
-		self.addSection(_("Moving Camera"))
-		self.addComponent(_('Right click'), _('Rotate the camera (drag the mouse)'))
-		self.addComponent('Shift + ' + _('Right click'), _('Translate the camera (drag the mouse)'))
-		self.addSection(_("Rotate Tool"))
-		self.addComponent(_('Left click'), _('Rotate the object (drag the mouse along a circle)'))
-		self.addComponent('Shift + ' + _('Left click'), _('Rotate the object slowly (drag the mouse along a circle)'))
-		self.addSection(_("Scale Tool"))
-		self.addComponent(_('Left click'), _("Modify the object's size (drag the mouse from the end of an axis)"))
-		self.addComponent('Shift + ' + _('Left click'), _("Modify the object's size slowly (drag the mouse from the end of an axis)"))
-		self.addComponent('Ctrl + ' + _('Left click'), _("Stretch the object (drag the mouse from the end of an axis)"))
-		self.addComponent('Ctrl + ' + 'Shift + ' + _('Left click'), _("Stretch the object slowly (drag the mouse from the end of an axis)"))
-		self.addSection(_("Translate Tool"))
-		self.addComponent(_('Left click'), _("Translate the object following an axis (drag the mouse from the end of an axis)"))
-		self.addComponent('Shift + ' + _('Left click'), _("Translate slowly the object following an axis (drag the mouse from the end of an axis)"))
+		s.Add(wx.StaticText(p, -1, _("If you have noticed a problem, please first ensure you are using the latest version of Cura By Dagoma.")), flag=wx.ALIGN_CENTER|wx.BOTTOM|wx.LEFT|wx.RIGHT, border=5)
+		s.Add(wx.StaticText(p, -1, _("To report the problem to the staff, please follow the following link to create an Issue on GitHub.")), flag=wx.ALIGN_CENTER|wx.BOTTOM|wx.LEFT|wx.RIGHT, border=5)
 
 
 		self.Fit()
-
-	def addSection(self, section):
-		p = self.panel
-		s = p.GetSizer()
-		staticText = wx.StaticText(p, -1, section)
-		staticText.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
-		s.Add(wx.StaticLine(p), flag=wx.EXPAND|wx.BOTTOM|wx.LEFT|wx.RIGHT, border=5)
-		s.Add(staticText, flag=wx.ALIGN_CENTRE|wx.BOTTOM|wx.TOP|wx.LEFT|wx.RIGHT, border=5)
-
-	def addComponent(self, shortcut, description):
-		p = self.panel
-		s = p.GetSizer()
-		line = wx.BoxSizer(wx.HORIZONTAL)
-		shortcutLabel = wx.StaticText(p, -1, shortcut)
-		shortcutLabel.SetFont(wx.Font(8, wx.SWISS, wx.NORMAL, wx.BOLD))
-		line.Add(shortcutLabel)
-		descriptionLabel = wx.StaticText(p, -1, ' : ' + description)
-		line.Add(descriptionLabel)
-		s.Add(line, flag=wx.LEFT|wx.RIGHT, border=5)
 
 	def OnClose(self, e):
 		self.Destroy()
