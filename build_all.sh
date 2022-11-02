e687b77b85476b0730a1b3c8528914889e38b4ad
orel <orel@dagoma.fr>
2022-11-02 13:14:09 +0100

    Update packaging scripts

--- build_all.sh
+++ build_all.sh
@@ -3,18 +3,18 @@
 BUILD_OS=${1:-w} # w = windows, m = mac, l = linux
 
 git pull
-for b in discoeasy200 explorer350 neva
-do
-	if [ "$BUILD_OS" = "l" ]; then
-		./package.sh $b debian 32
-		./package.sh $b debian 64
-		./package.sh $b archive 32
-		./package.sh $b archive 64
-	elif [ "$BUILD_OS" = "w" ];	then
-		./package.sh $b win32
-	elif [ "$BUILD_OS" = "m" ];	then
-		./package.sh $b darwin
-	fi
-done
+if [ "$BUILD_OS" = "l" ]; then
+	./package.sh debian 32 1
+	./package.sh debian 64 1
+	./package.sh archive 32 1
+	./package.sh archive 64 1
+elif [ "$BUILD_OS" = "w" ]; then
+	./package.sh windows 32 1
+	./package.sh windows 64 1
+elif [ "$BUILD_OS" = "m86" ]; then
+	./package.sh darwin x86 1
+elif [ "$BUILD_OS" = "m64" ]; then
+	./package.sh darwin arm64 1
+fi
 
 exit 0
